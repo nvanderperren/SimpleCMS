4301d6cdb10a5dceed062eb3896b98dace8f63fc
nvanderperren <ik.ben.nas@gmail.com>
2018-08-03 12:47:51 +0200

    Add layout for CollectionTableView

--- SimpleCMS/ViewControllers/HeritageCollectionTableViewController.swift
+++ SimpleCMS/ViewControllers/HeritageCollectionTableViewController.swift
@@ -36,7 +36,7 @@ class HeritageCollectionTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return heritageObjects.count
+        return 20
     }
 
     
