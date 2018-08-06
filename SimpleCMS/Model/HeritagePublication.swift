394d7be58b4baa96fa73cae9972e8645afe31e70
nvanderperren <ik.ben.nas@gmail.com>
2018-08-06 23:29:04 +0200

    Add publication class

--- SimpleCMS/Model/HeritagePublication.swift
+++ SimpleCMS/Model/HeritagePublication.swift
@@ -7,3 +7,75 @@
 //
 
 import Foundation
+
+
+class HeritagePublication {
+    
+    // MARK: Properties
+
+    
+    var id: Identification
+    var numberOfPages: Int?
+    var isPartOfSeries: Bool
+    var publication: Publication?
+    var contents: Contents?
+    var acquisition: Acquisition
+    var rights: Rights
+    var record: Record
+    
+    // MARK: Initializers
+    
+    init(id: Identification, isPartOfSeries: Bool = false, acquisition: Acquisition, rights: Rights, record: Record){
+        self.id = id
+        self.isPartOfSeries = isPartOfSeries
+        self.acquisition = acquisition
+        self.rights = rights
+        self.record = record
+    }
+    
+    // MARK: Internal structs
+    
+    struct Identification {
+        var idNumber: String
+        var author: [Person]
+        var title: String
+    }
+    
+    struct Publication {
+        var placeOfPublication: Location?
+        var publisher: [Person]?
+        var dateOfPublication: Date?
+    }
+    
+    struct Contents {
+        var genre: String?
+        var shortDescription: String?
+    }
+    
+    struct Acquisition {
+        var method: String //lijst
+        var date: Date
+        var source: String
+    }
+    
+    struct Rights {
+        var status: String //lijst
+        var creditline: String
+    }
+    
+    struct Record {
+        var date = Date()
+    }
+    
+    struct Person {
+        var name: String
+    }
+    
+    struct Location {
+        var municipality: String //lijst
+        var postalCode: String? //lijst
+        var street: String? //lijst
+        var houseNumber: String? //lijst
+
+    }
+}
--- SimpleCMS/Model/Seeder.swift
+++ SimpleCMS/Model/Seeder.swift
@@ -15,7 +15,7 @@ struct Seeder {
         collection.append(HeritageObject(id: "TILE1", name: "armband", category: HeritageObjectCategory.metalDetectingFind, photos: [UIImage(named:"armband")]))
         collection.append(HeritageObject(id: "TILE2", name: "munitie", category: HeritageObjectCategory.metalDetectingFind, photos: [UIImage(named:"munitie")]))
         collection.append(HeritageObject(id: "TILE3", name: "een oud boek", category: HeritageObjectCategory.publication, photos: [UIImage(named:"boek")]))
-        collection.append(HeritageObject(id: "TILE4", name: "een schilderij", category: HeritageObjectCategory.artwork, photos: [UIImage(named:"schilderij")]))
+        collection.append(HeritageObject(id: "TILE4", name: "een schilderij", category: HeritageObjectCategory.artefact, photos: [UIImage(named:"schilderij")]))
         collection.append(HeritageObject(id: "TILE5", name: "een muziekinstrument", category: HeritageObjectCategory.artefact, photos: [UIImage(named: "kalimba")]))
         return collection
     }
