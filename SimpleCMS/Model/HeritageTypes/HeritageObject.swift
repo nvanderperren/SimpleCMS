57202dde3ccf6082b6454ff7a4ce94c0b50a36d5
nvanderperren <ik.ben.nas@gmail.com>
2018-08-15 23:38:32 +0200

    Add first realm object

--- SimpleCMS/Model/HeritageTypes/HeritageObject.swift
+++ SimpleCMS/Model/HeritageTypes/HeritageObject.swift
@@ -15,27 +15,16 @@ import RealmSwift
     // source: https://www.projectcest.be/wiki/Publicatie:PACKED_Dataprofielen
     dynamic var id: String = ""
     dynamic var name: String = ""
-    dynamic var category: HeritageObjectCategory.RawValue = "artefact"
-//    var license: String? // enum van maken
-//    var currentLocation: String? // klasse van maken met eigenschappen.
+    dynamic var category: String = ""
     dynamic var photo: String? = nil
-//    var objectDescription: String?
-//    var dimensions: [ObjectDimension]? = []
     
     // MARK: - Initializers
-//    init(id: String, name: String, category: HeritageObjectCategory, photo: String = "defaultPhoto") {
-//        self.id = id
-//        self.name = name
-//        self.category = category
-//        self.photo = photo
-//    }
-//
-//    init(id: String, name: String, category: HeritageObjectCategory, location: String, license: String){
-//        self.id = id
-//        self.name = name
-//        self.category = category
-//        self.currentLocation = location
-//        self.license = license
-//    }
+    convenience init(id: String, name: String, category: HeritageObjectCategory, photo: String?){
+        self.init()
+        self.id = id
+        self.name = name
+        self.category = category.rawValue
+        self.photo = photo
+    }
     
 }
